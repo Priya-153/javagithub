3d882eed8a85f6ab519606c388f209b8a20561e9
Priya-153 <4al24cs153@aiet.org.in>
2026-04-21 17:53:50 +0530

    Add files via upload

---
+++ ColorSearch.java
@@ -0,0 +1,24 @@
+package Com.Colors;
+
+import java.util.ArrayList;
+
+public class ColorSearch {
+    public static void main(String[] args) {
+
+        ArrayList<String> colors = new ArrayList<>();
+
+        colors.add("Blue");
+        colors.add("Green");
+        colors.add("Yellow");
+        colors.add("Red");
+        colors.add("White");
+
+        System.out.println("List of colors: " + colors);
+
+        if (colors.contains("Red")) {
+            System.out.println("Red color is available in the list.");
+        } else {
+            System.out.println("Red color is NOT available in the list.");
+        }
+    }
+}
